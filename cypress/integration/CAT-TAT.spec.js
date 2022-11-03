6eb2f14521943e453daf40674fcfedbc4a56338e
Tulio Lascalla <tulio@codebit.com.br>
2022-11-03 16:20:36 -0300

    desafio find cat

--- cypress/integration/CAT-TAT.spec.js
+++ cypress/integration/CAT-TAT.spec.js
@@ -193,5 +193,11 @@ describe('Central de Atendimento ao Cliente TAT', function() {
                 expect(body).to.include('CAC TAT')//body inclui o testo
             })
     })
+    it.only('desafio - encontra o gato escondido', function (){
+        cy.get('#cat')
+            .invoke('show').should('be.visible')
+        cy.get('#title').invoke('text', 'CAT TAT')
+        cy.get('#subtitle').invoke('text', 'Eu amo gatos')
+    })
 
 })
