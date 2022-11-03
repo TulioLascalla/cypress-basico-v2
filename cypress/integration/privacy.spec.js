2b29a14c98bedb34844b788d60325d7b693a052f
Tulio Lascalla <tulio@codebit.com.br>
2022-11-03 11:45:30 -0300

    adjust name of test

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,4 +1,4 @@
-it('', function () {
+it('teste a página da politica de privacidade de forma independente', function () {
     cy.visit("./src/privacy.html")
     cy.contains('CAC TAT - Política de privacidade').should("be.visible")//confere que contem a string
 });
